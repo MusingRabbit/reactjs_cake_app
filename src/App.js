b8aa7a6ce4ae9dde13beeeca1ad12d93ca9f34e2
MusingRabbit <scramuk@gmail.com>
2018-02-15 09:30:26 +0000

    Added Components : CakeComponent, ViewCakes, AddCake

--- src/App.js
+++ src/App.js
@@ -1,83 +1,33 @@
 import React, { Component } from 'react';
 import Axios from 'axios'
 import './App.css';
+import ViewCakes from './ViewCakes'
+import AddCake from './AddCake'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
-  constructor(){
-    super();
-
-    this.getCakes = this.getCakes.bind(this);
-
-    this.state = {
-      pageTitle : "It's all about cakes!",
-      apiUrl : "http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/",
-      cakes : []
+    constructor(){
+      super();
     }
-  }
 
-  getCakes(response){
-    this.setState({
-      cakes : response.data
-    });
-  }
-
-  componentWillMount(){
-    // Called the first time the component is loaded and added onto the page
+    render() {
+      return (
+        <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
     
-    Axios.get(this.state.apiUrl + 'cakes').then(this.getCakes);
-  }
-
-  componentDidMount(){
-    // Called after the component has been rendered onto the page
-
+          <hr />
     
-  }
-
-  componentWillReceiveProps(nextProps){
-    // Called when properties have been set/changed for this component.
-  }
-
-  componentWillUpdate(nextProps, nextState){
-    // Called when the properties and/or state has changed
-  }
-
-  componentWillUnmount(){
-    // Called when the component will be unloaded/removed from page
-  }
-
-
-  render() {
-
-    let cakes = this.state.cakes;
-
-    return (
-
-      <div className="App">
-        <form ref="cakeForm">
-          <div className="container-fluid" ref="container">
-            <div>
-              <h1>{this.state.pageTitle}</h1>
-            </div>
-            <div>{
-              cakes.map(x=> 
-                <div key={x.id} className="row">
-                  <div className='col-md-3  text-left'>
-                    <div className='panel panel-default'>
-                      <div className='panel-heading'/>
-                        {x.name}
-                      </div>
-                      <div className='panel-body'/>
-                          <img src={x.imageUrl} className="img-responsive rounded float-right"/>
-                      </div>
-                    </div>
-              )
-            }
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+          <Route exact path="/" component={ViewCakes} />
+          <Route exact path="/addCake" component={AddCake} />
+        </div>
+      </Router>
+      );
+    }
 }
 
 export default App;
